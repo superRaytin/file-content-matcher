23a4aa136ba4b0006c5fd1c266415b1ca088bc6b
Kaname <56084970+kaname-png@users.noreply.github.com>
2021-09-09 10:03:49 -0600

    feat(typescript): added types for typescript

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+export interface FileMatcherOptionsI {
+    path?: string;
+    recursiveDepth?: number;
+    readFileConcurrency?: number;
+    micromatchOptions?: any;
+    filter?: {
+      namePatterns?: string[];
+      contentRegExp?: string;
+      readOptions?: string;
+    };
+}
+
+export default class FileContentMatcher {
+    /**
+     * Search files
+     * @param options File search options.
+     */
+    public match(options: FileMatcherOptionsI): void;
+}
